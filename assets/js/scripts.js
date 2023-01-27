b092e0564e8b6ce2307efad277afb66f29f81f94
yus-torres <info@synthesisgroup.co>
2023-01-27 18:04:18 +0800

    Updates

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -8,3 +8,28 @@ $(window).scroll(function() {
 		$(".header").removeClass("shrink");
 	}
 });
+
+
+// Slider Awards
+$('.slider-awards').slick({
+	infinite: true,
+	slidesToShow: 5,
+	slidesToScroll: 1,
+	arrows: false,
+	responsive: [
+		{
+			breakpoint: 768,
+				settings: {
+				slidesToShow: 2,
+				slidesToScroll: 1
+			}
+		},
+		{
+			breakpoint: 320,
+				settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+		}
+	]
+});
